d3617acda392d0b26a7ae9f72895d0189b0be9f4
David Colatti <davidcolatti@gmail.com>
2020-12-12 09:58:56 -0500

    feat: new tool to make my life easier when filtered

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const fs = require("fs");
+const csvOrExcel = require("csv-excel-to-json");
+const { convertArrayToCSV } = require("convert-array-to-csv");
 
 const keywords = fs.readFileSync("keywords.txt").toString().split("\r\n");
 
-const main = async () => {};
+const writeJson = () => {
+  csvOrExcel.convertToJson("./data.csv", "./data.json");
+};
+
+const main = async () => {
+  writeJson();
+
+  let data;
+  await setTimeout(async () => {
+    data = require("./data.json");
+
+    const filteredData = data.filter((lead) => {
+      return !!keywords.find((word) => lead.category === word);
+    });
+
+    console.log(`Found ${filteredData.length} out of ${data.length}`);
+
+    const csv = await convertArrayToCSV(filteredData);
+
+    fs.writeFile("./output.csv", csv, () => {});
+  }, 500);
+};
 
 main();
